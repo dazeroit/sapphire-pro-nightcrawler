f0d9f37c036dce69d0f616c4120093a5b00de571
ivan.maruca <ivan.maruca@gmail.com>
2021-01-03 19:11:53 +0100

    fix endstop

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -989,7 +989,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { 0, -45, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { 0, -45, -0.6 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
